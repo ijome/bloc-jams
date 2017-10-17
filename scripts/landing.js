dd87b9d9bb63620c64a3858e59a091156ac9b9c3
ijome <gallowayalec@gmail.com>
2017-10-16 19:27:11 -0700

    link scripts/utilities.js to index.html, fix error in utilities.js for loop, refactor landing.js to incorporate forEach function from utilities.js

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,23 +1,20 @@
-var points = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
+
+var revealPoints = function(point) {
+  point.style.opacity = 1;
+  point.style.transform = "scaleX(1) translateY(0)";
+  point.style.msTransform = "scaleX(1) translateY(0)";
+  point.style.WebkitTransform = "scaleX(1) translateY(0)";
+};
 
 var animatePoints = function (points) {
-
-  var revealPoints = function() {
-      for (var i = 0; i < points.length; i++) {
-        points[i].style.opacity = 1;
-        points[i].style.transform = "scaleX(1) translateY(0)";
-        points[i].style.msTransform = "scaleX(1) translateY(0)";
-        points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-      };
-  };
-
-  revealPoints();
+  forEach(points, revealPoints);
 };
 
 window.onload = function() {
 //automatically call animatePoints if the window is taller than 950px
   if (window.innerHeight > 950) {
-    animatePoints(points);
+    animatePoints(pointsArray);
   }
 
 var sellingPoints = document.getElementsByClassName('selling-points')[0];
@@ -25,7 +22,7 @@ var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHei
 
   window.addEventListener('scroll', function(event) {
     if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-      animatePoints(points);
+      animatePoints(pointsArray);
     }
   });
 }
