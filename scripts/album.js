1f35cb1d9781751426ff4fd76a535d8807b1b4da
ijome <gallowayalec@gmail.com>
2017-10-19 12:34:15 -0700

    remove static code from album.html and create two example templates in album.js

---
+++ scripts/album.js
@@ -0,0 +1,30 @@
+//Example Album
+var albumPicasso = {
+  title: 'The Colors',
+  artist: 'Pablo Picaso',
+  label: 'Custom',
+  year: '1881',
+  albumArtUrl: 'assets/images/album_covers/01.jpg',
+  songs:  [
+    { title: 'Blue', duration: '4:26' }
+    { title: 'Green', duration: '3:14' }
+    { title: 'Red', duration: '5:01' }
+    { title: 'Pink', duration: '3:21' }
+    { title: 'Magenta', duration: '2:15' }
+  ]
+};
+// Another Example Album
+var albumMacaroni = {
+  title: 'The Telephone',
+  artist: 'Guglielmo Macaroni',
+  label: 'EM',
+  year: '1909',
+  albumArtUrl: 'assets/images/album_covers/20.png',
+  songs: [
+    { title: 'Hello, Operator?', duration: '1:01' }
+    { title: 'Ring, ring, ring', duration: '5:01' }
+    { title: 'Fits in your pocket', duration: '3:21' }
+    { title: 'Can you hear me now?', duration: '3:14' }
+    { title: 'Wrong phone number', duration: '2:15' }
+  ]
+};
