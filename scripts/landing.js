ac9cdc26dcd20d4db842db44eabf4d11a76ac4a1
ijome <gallowayalec@gmail.com>
2017-11-07 18:31:56 -0800

    refactor landing.js w/ jQuery libraries

--- scripts/landing.js
+++ scripts/landing.js
@@ -11,18 +11,17 @@ var animatePoints = function (points) {
   forEach(points, revealPoints);
 };
 
-window.onload = function() {
+$(window).load(function() {
 //automatically call animatePoints if the window is taller than 950px
-  if (window.innerHeight > 950) {
-    animatePoints(pointsArray);
+  if ($(window).height() > 950) {
+    animatePoints();
   }
 
-var sellingPoints = document.getElementsByClassName('selling-points')[0];
-var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200
+var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
 
-  window.addEventListener('scroll', function(event) {
-    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-      animatePoints(pointsArray);
+  $(window).scroll(function(event) {
+    if ($(window).scrollTop() >= scrollDistance) {
+      animatePoints();
     }
   });
-}
+});
