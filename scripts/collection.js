a6fe09800065e85a552e8bbfd6e69d1a1769ce84
ijome <gallowayalec@gmail.com>
2017-11-09 16:38:13 -0800

    refactor collection.js

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,18 +1,23 @@
-var collectionItemTemplate =
-'<div class="collection-album-container column fourth">'
-+'  <img src="assets/images/album_covers/01.png">'
-+'  <div class="collection-album-info caption">'
-+'    <p>'
-+'      <a class="album-name" href="album.html">The Colors</a>'
-+'      <br/>'
-+'      <a href="album.html">Pablo Picaso</a>'
-+'      <br/>'
-+'      X Songs'
-+'      <br/>'
-+'    </p>'
-+'  </div>'
-+'</div>'
-;
+var buildcollectionItemTemplate = function () {
+  var template =
+   '<div class="collection-album-container column fourth">'
+  +'  <img src="assets/images/album_covers/01.png">'
+  +'  <div class="collection-album-info caption">'
+  +'    <p>'
+  +'      <a class="album-name" href="album.html">The Colors</a>'
+  +'      <br/>'
+  +'      <a href="album.html">Pablo Picaso</a>'
+  +'      <br/>'
+  +'      X Songs'
+  +'      <br/>'
+  +'    </p>'
+  +'  </div>'
+  +'</div>'
+  ;
+
+  return $(template);
+};
+
 
 window.onload = function() {
   var collectionContainer = document.getElementsByClassName('album-covers')[0];
