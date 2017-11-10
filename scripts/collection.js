42295ccedf8462082c8956c608ed936ef6373782
ijome <gallowayalec@gmail.com>
2017-11-09 16:49:16 -0800

    refactor append function in collection.js with jQuery

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,4 +1,4 @@
-var buildcollectionItemTemplate = function () {
+var buildCollectionItemTemplate = function () {
   var template =
    '<div class="collection-album-container column fourth">'
   +'  <img src="assets/images/album_covers/01.png">'
@@ -19,11 +19,12 @@ var buildcollectionItemTemplate = function () {
 };
 
 
-window.onload = function() {
-  var collectionContainer = document.getElementsByClassName('album-covers')[0];
-  collectionContainer.innerHTML = '';
+$(window)load(function() {
+  var $collectionContainer = ('.album-covers');
+  $collectionContainer.empty();
 
   for (var i = 0; i < 12; i++) {
-    collectionContainer.innerHTML += collectionItemTemplate;
+    var $newThumbnail = buildCollectionItemTemplate();
+    $collectionContainer.append($newThumbnail);
   }
-}
+});
